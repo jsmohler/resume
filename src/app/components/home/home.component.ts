f71581c91e7850e162df68596af313435af3d919
Jordan Mohler <Jordan.Mohler@allata.com>
2020-08-27 22:55:25 -0600

    Clean up

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 
-interface TypewriterContent {
-  text: string;
-  backspace: boolean;
-}
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
