4cc38aca6df8ad111be90dd36d6dd3c786e30843
Jordan Mohler <Jordan.Mohler@allata.com>
2020-08-30 14:05:36 -0600

    Publications

--- src/app/components/publications/publications.component.ts
+++ src/app/components/publications/publications.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-publications',
   templateUrl: './publications.component.html',
-  styleUrls: ['./publications.component.css']
+  styleUrls: ['./publications.component.css'],
 })
 export class PublicationsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  navTo(url: string): void {
+    window.location.href = url;
   }
-
 }
