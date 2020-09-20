ad1e5d6c8890af06dba02bb0c4c2ee33fc0c91e1
Jordan Mohler <jordansmohler@gmail.com>
2020-09-20 13:41:27 -0600

    Fill out education section

--- src/app/layout/ant.module.ts
+++ src/app/layout/ant.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzPopoverModule } from 'ng-zorro-antd';
 
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
@@ -10,24 +9,10 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { NzStepsModule } from 'ng-zorro-antd/steps';
-import { NzSelectModule } from 'ng-zorro-antd/select';
 
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzCollapseModule } from 'ng-zorro-antd/collapse';
-import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
-import { NzEmptyModule } from 'ng-zorro-antd/empty';
 import { NzListModule } from 'ng-zorro-antd/list';
-import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-
-import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzAffixModule } from 'ng-zorro-antd/affix';
@@ -35,7 +20,6 @@ import { NzRateModule } from 'ng-zorro-antd/rate';
 import { NzTimelineModule } from 'ng-zorro-antd/timeline';
 
 const antModules = [
-  NzPopoverModule,
   NzButtonModule,
   NzIconModule,
   NzTypographyModule,
@@ -44,21 +28,9 @@ const antModules = [
   NzDropDownModule,
   NzMenuModule,
   NzPageHeaderModule,
-  NzStepsModule,
-  NzSelectModule,
   NzAvatarModule,
-  NzBadgeModule,
   NzCardModule,
-  NzCollapseModule,
-  NzDescriptionsModule,
-  NzEmptyModule,
   NzListModule,
-  NzTabsModule,
-  NzTagModule,
-  NzDrawerModule,
-  NzModalModule,
-  NzSkeletonModule,
-  NzSpinModule,
   NzAffixModule,
   NzDividerModule,
   NzRateModule,
