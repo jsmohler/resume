cfb77f6b6073618ac85a28f3d8e0eedbacffd1b6
Jordan Mohler <jordansmohler@gmail.com>
2020-09-28 19:56:30 -0600

    Copy cleanup

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,15 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-  isMobile: boolean;
-  readonly mobileBreakpoint = 400;
-
   constructor() {}
 
-  ngOnInit(): void {
-    this.isMobile = screen.width < this.mobileBreakpoint;
-    window.onresize = () => {
-      this.isMobile = screen.width < this.mobileBreakpoint;
-    };
-  }
+  ngOnInit(): void {}
 }
